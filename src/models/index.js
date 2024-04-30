2cd7a2937eecb326ff992d9fef994b60e40d5b00
Ivy Hernández <hernandezlopezivy5@gmail.com>
2024-04-30 02:06:44 -0600

    cart

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,14 @@
 const User = require("./User")
 const Category = require("./Category")
 const Product = require("./Product")
+const Cart = require("./Cart")
 
 //relaciones
 Product.belongsTo(Category)
 Category.hasMany(Product)
+
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
